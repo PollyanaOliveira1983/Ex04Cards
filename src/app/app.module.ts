4249d201037d2a47cfcb03384e52101f31d1bc21
PollyanaOliveira1983 <pollyana.oliveira@gmail.com>
2022-03-09 22:01:55 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material.module';
+import { CardComponent } from './card/card.component';
+import { ToogleComponent } from './toogle/toogle.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardComponent,
+    ToogleComponent,
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+
+    /* MatSnackBarModule
+ */
   ],
   providers: [],
   bootstrap: [AppComponent]
